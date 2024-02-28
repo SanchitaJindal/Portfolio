71f15e003ad28a435776b759a09693386a030933
arjunbector <arjunbector@gmail.com>
2024-02-28 16:12:20 +0530

    add form

--- js/app.js
+++ js/app.js
@@ -1,28 +1,40 @@
+const cursor = document.querySelector(".cursor");
 
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
 
-const cursor = document.querySelector('.cursor');
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
 
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-})
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
 
-document.addEventListener('click', () => {
-    cursor.classList.add("expand");
+let count = localStorage.getItem("visitorCount");
+if (!count) {
+  count = 0;
+}
+function updateCount() {
+    document.getElementById("visitorCount").innerText = `Visitors: ${count}`;
+}
+window.onload = function () {
+    count++;
+    localStorage.setItem("visitorCount", count);
+    updateCount();
+};
 
+const formSubmitMsg = document.getElementById("form-submit-success");
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formSubmitMsg.classList.add("show");
     setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500)
+        formSubmitMsg.classList.remove("show");
+    }, 3000);
+    form.reset();
 })
-
-let count = localStorage.getItem('visitorCount');
-        if (!count) {
-            count = 0;
-        }
-        function updateCount() {
-            document.getElementById('visitorCount').innerText = `Visitors: ${count}`;
-        }
-        window.onload = function() {
-            count++;
-            localStorage.setItem('visitorCount', count);
-            updateCount();
-        };
